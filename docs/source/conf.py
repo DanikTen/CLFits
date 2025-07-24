4e61492341b2f9a9f8c3215f20d6009f3b36a73f
Amber <ambermalpas@gmail.com>
2025-07-24 18:51:06 -0400

    fix(build): Update release variable in docs and bumpversion config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath("../../src"))
 project = "CLFits"
 copyright = "2024, Amber Malpas"
 author = "Amber Malpas"
-release = "0.1.3"
+release = "0.1.4"
 version = "0.1.4"
 
 # -- General configuration ---------------------------------------------------
