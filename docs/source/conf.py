2043960e15c9c2d37f4bb08b206c0303870c8ccc
Amber <ambermalpas@gmail.com>
2025-07-24 18:22:01 -0400

    chore(release): Manually bump version to 0.1.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,8 @@ sys.path.insert(0, os.path.abspath("../../src"))
 project = "CLFits"
 copyright = "2024, Amber Malpas"
 author = "Amber Malpas"
-release = "0.1.0"
-version = "0.1.0"
+release = "0.1.3"
+version = "0.1.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
